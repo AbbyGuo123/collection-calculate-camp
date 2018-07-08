cf6374b21ea0edad9c60658ce4453ef2e8db120c
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-08 14:27:18 +0800

    3

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,25 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let number_c = [];
+  if(number_a<number_b){
+    for(let i=number_a;i<=number_b;i++){
+      if(i%2===0)
+      number_c.push(i);
+    }
+  }
+  else if(number_a>number_b){
+    for(let i=number_a;i>=number_b;i--){
+      if(i%2===0)
+      number_c.push(i);
+    }
+  }
+  else{
+    if(number_a%2===0)
+    number_c.push(number_a);
+    
+  }
+  return number_c;
 }
 
 module.exports = get_integer_interval_2;
