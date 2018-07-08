3edf9644c18eed7a2ca73f6fece6abd9df2c7100
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-08 14:18:35 +0800

    2

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function collect_last_element(collection) {
-  //在这里写入代码
+  let lastObject = collection[collection.length-1];
+  return lastObject;
 }
 
 module.exports = collect_last_element;
