077a55c0de0ab8118e3e68c4b9042ff7a9f870ae
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-08 14:16:40 +0800

    1

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,6 +1,13 @@
 'use strict';
 
 function collect_all_even(collection) {
+  var collection_b =[];
+  for(let collectionObject of collection){
+    if(collectionObject%2==0)
+    collection_b.push(collectionObject);
+  }
+   
+   return collection_b;
   //在这里写入代码
 }
 
